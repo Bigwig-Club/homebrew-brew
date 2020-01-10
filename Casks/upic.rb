92843abdf55e8fa7bfb862a05357b5a4abd320d8
m01i0ng <alley.ma@qq.com>
2020-01-10 19:26:29 +0800

    Setup ci

--- Casks/upic.rb
+++ Casks/upic.rb
@@ -1,19 +1,18 @@
 cask 'upic' do
-    version '0.16.4'
-    sha256 '972da76944ed78591a450eaf9dd00642b3d4ca2cece0b6700c72c2d36ce49c4b'
-  
-    url "https://github.com/gee1k/uPic/releases/download/v#{version}/uPic.dmg"
-    appcast 'https://github.com/gee1k/uPic/releases.atom'
-    name 'uPic'
-    homepage 'https://github.com/gee1k/uPic'
-  
-    depends_on macos: '>= :sierra'
-  
-    app 'uPic.app'
-  
-    zap trash: [
-                 '~/Library/Preferences/com.svend.uPic.plist',
-                 '~/Library/Caches/com.svend.uPic',
-               ]
-  end
-  
+  version '0.16.4'
+  sha256 '972da76944ed78591a450eaf9dd00642b3d4ca2cece0b6700c72c2d36ce49c4b'
+
+  url "https://github.com/gee1k/uPic/releases/download/v#{version}/uPic.dmg"
+  appcast 'https://github.com/gee1k/uPic/releases.atom'
+  name 'uPic'
+  homepage 'https://github.com/gee1k/uPic'
+
+  depends_on macos: '>= :sierra'
+
+  app 'uPic.app'
+
+  zap trash: [
+               '~/Library/Preferences/com.svend.uPic.plist',
+               '~/Library/Caches/com.svend.uPic',
+             ]
+end
