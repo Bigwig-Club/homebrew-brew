b76e940e5468bd6c50b39d69c4fefec1098b1371
null <uclort@gmail.com>
2020-01-16 09:51:06 +0800

    Update openfile.rb

--- Casks/openfile.rb
+++ Casks/openfile.rb
@@ -1,6 +1,6 @@
 cask 'openfile' do
-  version '1.3.2'
-  sha256 '1f805f89039358e1f4d3c9f37d7f96819dc312e0e99de3f74e147c85ab236cf6'
+  version '1.3.3'
+  sha256 '70f64214f3e416b6aa2b4d83d3259c35181a2b86511694af508a8145ec0ccaae'
 
   url "https://github.com/nlnlnull/OpenFile/releases/download/V#{version}/OpenFile.zip"
   appcast 'https://github.com/nlnlnull/OpenFile/releases.atom'
