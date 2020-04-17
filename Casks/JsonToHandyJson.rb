3df48700d0ff03b91d4081c00b6fab1d9baad601
null <uclort@gmail.com>
2020-04-17 16:25:23 +0800

    Create JsonToHandyJson.rb

---
+++ Casks/JsonToHandyJson.rb
@@ -0,0 +1,16 @@
+cask 'jsontohandyjson' do
+  version '10'
+  sha256 '6c6a42012623abcc4b33c948fddc627321e3db7e35cf7f8e55beb8e239339b5c'
+
+  url "https://github.com/nlnlnull/JsonToHandyJson/releases/download/V#{version}/JsonToHandyJson.zip"
+  appcast 'https://github.com/nlnlnull/JsonToHandyJson/releases.atom'
+  name 'JsonToHandyJson'
+  homepage 'https://github.com/nlnlnull/JsonToHandyJson'
+
+  depends_on macos: '>= :sierra'
+
+  app 'JsonToHandyJson.app'
+
+  zap trash: [
+             ]
+end
