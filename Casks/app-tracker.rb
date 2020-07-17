5585206dce8052f2d9e166c3b29e4deeb08fd6f8
Licardo <1014660822@qq.com>
2020-07-17 19:15:41 +0800

    Update app-tracker.rb

--- Casks/app-tracker.rb
+++ Casks/app-tracker.rb
@@ -1,9 +1,9 @@
 
 cask 'app-tracker' do
-  version '1.0.0'
-  sha256 'e1340f9dd0ab81c9b9c1ebcb36b3659b3c5900f910545e4bf3068c3a7e756ad9'
+  version '1.0.1'
+  sha256 '9771d7de43fdcd8f6a12b940597039d8c47e78523f83378880dedde211cea46a'
 
-  url "https://github.com/L1cardo/AppTracker-Release/releases/download/v#{version}/App.Tracker.dmg"
+  url "https://github.com/L1cardo/AppTracker-Release/releases/download/v#{version}/App.Tracker.zip"
   appcast 'https://github.com/L1cardo/AppTracker-Release/releases.atom'
   name 'App Tracker'
   homepage 'https://github.com/L1cardo/AppTracker-Release'
@@ -15,6 +15,6 @@ cask 'app-tracker' do
   zap trash: [
                '~/Library/Preferences/com.licardo.AppTracker.plist',
                '~/Library/Caches/com.licardo.AppTracker',
-               '~/Library/Containers/com.licardo.AppTracker'
+               '~/Library/Containers/com.licardo.AppTracker',
              ]
 end
