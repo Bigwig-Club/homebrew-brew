e4944fd802b1763505e85afcaf45d42fa8f793a9
Licardo <1014660822@qq.com>
2020-09-19 21:16:27 +0800

    Update account-switcher.rb

--- Casks/account-switcher.rb
+++ Casks/account-switcher.rb
@@ -1,6 +1,6 @@
 cask "account-switcher" do
-  version "0.0.4"
-  sha256 "51082067e4d590abf03ec697b8d52c18882925fe82595453f417c3a8c0611a28"
+  version "0.0.5"
+  sha256 "89d0a5bdcd00532cbbaafd5de6b33d41586dce2c71650ee8531bc64f6f1418c0"
 
   url "https://github.com/Bigwig-Club/Account-Switcher/releases/download/v#{version}/Account.Switcher.zip"
   appcast "https://github.com/Bigwig-Club/Account-Switcher/releases.atom"
