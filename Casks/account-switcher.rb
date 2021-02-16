1b863a237c89a26600900362a3942d1137c2fc16
Licardo <1014660822@qq.com>
2021-02-16 15:53:55 +0800

    Update account-switcher.rb

--- Casks/account-switcher.rb
+++ Casks/account-switcher.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 cask "account-switcher" do
-  version "0.1.1"
-  sha256 "84da8d9f28e9c9f9329dddca1cf4826c6d163b71e7971764dfe39c2ce24f5e22"
-
+  if MacOS.version >= :big_sur
+    version "1.0.0"
+    sha256 "f127a3a60f2552e57fb72fa5dd4bb90daf630bf972c0d7c3f16a9e98feeefbb7"
+  else
+    version "0.1.1"
+    sha256 "84da8d9f28e9c9f9329dddca1cf4826c6d163b71e7971764dfe39c2ce24f5e22"
+  end
+  
   url "https://github.com/Bigwig-Club/Account-Switcher/releases/download/v#{version}/Account.Switcher.zip"
   appcast "https://github.com/Bigwig-Club/Account-Switcher/releases.atom"
   name "Account Switcher"
   homepage "https://github.com/Bigwig-Club/Account-Switcher"
-
   depends_on macos: ">= :catalina"
 
   app "Account Switcher.app"
