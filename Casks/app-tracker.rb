a89b046693beb6be85f9e7baa0e76253024afd78
Licardo <1014660822@qq.com>
2022-03-21 12:42:30 +0800

    Update app-tracker.rb

--- Casks/app-tracker.rb
+++ Casks/app-tracker.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 cask "app-tracker" do
-  version "1.0.1"
-  sha256 "9771d7de43fdcd8f6a12b940597039d8c47e78523f83378880dedde211cea46a"
+  version "1.0.0"
+  sha256 "d2ae0edcf4c015330df310439389ecb0c70ed6c3f778644a1367a8376df63913"
 
   url "https://github.com/L1cardo/AppTracker-Release/releases/download/v#{version}/App.Tracker.zip"
   appcast "https://github.com/L1cardo/AppTracker-Release/releases.atom"
   name "App Tracker"
   homepage "https://github.com/L1cardo/AppTracker-Release"
 
-  depends_on macos: ">= :catalina"
+  depends_on macos: ">= :monterey"
 
   app "App Tracker.app"
 
