ff56b94dcd98de7aa5af5eade07c1f6b47533328
null <uclort@gmail.com>
2022-04-16 20:28:37 +0800

    update appinfo 0.0.3

--- Casks/appInfo.rb
+++ Casks/appInfo.rb
@@ -2,7 +2,7 @@
 
 cask "appinfo" do
   version "0.0.2"
-  sha256 "2448ed3423b785499cbf853e88144d720fbffde831f9929ef20802e19f840409"
+  sha256 "1aae6c709f955e122f67081c2c1b6991322b771b3d6accc59dfcd2248a1ee8a2"
 
   url "https://github.com/uclort/AppInfo-Publish/releases/download/#{version}/AppInfo.zip"
   appcast "https://github.com/uclort/AppInfo-Publish/releases.atom"
@@ -13,7 +13,7 @@ cask "appinfo" do
 
   app "AppInfo.app"
 
-  # zap trash: [
-  #   "~/Library/Preferences/houmeng.OpenFile.plist",
-  # ]
+  zap trash: [
+    "~/Library/Containers/uclort.com.AppInfo-macOS/Data/Library/Preferences/uclort.com.AppInfo-macOS.plist",
+  ]
 end
