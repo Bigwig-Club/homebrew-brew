2efc5cec41d46d7ea7e7e455badfbd45a446a44d
Uclort <uclort@gmail.com>
2022-04-16 21:46:45 +0800

    update appinfo sha256

--- Casks/appinfo.rb
+++ Casks/appinfo.rb
@@ -2,7 +2,7 @@
 
 cask "appinfo" do
   version "0.0.3"
-  sha256 "1aae6c709f955e122f67081c2c1b6991322b771b3d6accc59dfcd2248a1ee8a2"
+  sha256 "bd337063a46d49bb6d06c547101980d02692b75d5ab5e11a66f33bbbdc436995"
 
   url "https://github.com/uclort/AppInfo-Publish/releases/download/#{version}/AppInfo.zip"
   appcast "https://github.com/uclort/AppInfo-Publish/releases.atom"
