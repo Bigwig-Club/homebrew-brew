8c7974b7c4f27462330e12c7c06165ac25dd7b5e
Uclort <uclort@gmail.com>
2022-04-17 20:43:00 +0800

    update appinfo 0.0.6

--- Casks/appinfo.rb
+++ Casks/appinfo.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 cask "appinfo" do
-  version "0.0.5"
-  sha256 "2aea1803e99255827930209ddca9ecc21e818dca2bae1cf663ac8dced1e48c86"
+  version "0.0.6"
+  sha256 "62bc7b7975af36a79af7c87afbae8aa96a814c80bf8e34c7d5b7aff24cbbce77"
 
   url "https://github.com/uclort/AppInfo-Publish/releases/download/#{version}/AppInfo.zip"
   appcast "https://github.com/uclort/AppInfo-Publish/releases.atom"
