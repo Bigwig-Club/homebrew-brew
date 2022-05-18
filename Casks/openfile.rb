acaaf3b83412ab1d9942e21a149209e21246cc24
Uclort <uclort@gmail.com>
2022-05-18 19:32:01 +0800

    update reop

--- Casks/openfile.rb
+++ Casks/openfile.rb
@@ -4,10 +4,10 @@ cask "openfile" do
   version "1.3.4"
   sha256 "960d2c26e47989a493ca8a26851ebcbe3fd99a62c3733d4149a121cc66cac08e"
 
-  url "https://github.com/nlnlnull/OpenFile/releases/download/V#{version}/OpenFile.zip"
-  appcast "https://github.com/nlnlnull/OpenFile/releases.atom"
+  url "https://github.com/uclort/OpenFile/releases/download/V#{version}/OpenFile.zip"
+  appcast "https://github.com/uclort/OpenFile/releases.atom"
   name "OpenFile"
-  homepage "https://github.com/nlnlnull/OpenFile"
+  homepage "https://github.com/uclort/OpenFile"
 
   depends_on macos: ">= :sierra"
 
