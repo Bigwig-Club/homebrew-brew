a229395e7fbc9fbc7338b289398268c8d163c073
Uclort <uclort@gmail.com>
2023-07-27 10:28:24 +0800

    Update translation.rb

--- Casks/translation.rb
+++ Casks/translation.rb
@@ -5,11 +5,16 @@ cask "translation" do
   sha256 "e96fe9e37d98ef47757646822b702a778ad6d1697a35ea0f518e5c5a10a1a359"
 
   url "https://github.com/uclort/Translation/releases/download/V#{version}/Translation.zip"
-  appcast "https://github.com/uclort/Translation/releases.atom"
+  # appcast "https://github.com/uclort/Translation/releases.atom"
   name "Translation"
   homepage "https://github.com/uclort/Translation"
 
   depends_on macos: ">= :catalina"
+  
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Translation.app"
 end
