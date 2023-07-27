fc3b17cdfd582b9571065e5a3a7493d173fca9b5
Uclort <uclort@gmail.com>
2023-07-27 10:27:33 +0800

    Update app-tracker.rb

--- Casks/app-tracker.rb
+++ Casks/app-tracker.rb
@@ -5,13 +5,18 @@ cask "app-tracker" do
   sha256 "d2ae0edcf4c015330df310439389ecb0c70ed6c3f778644a1367a8376df63913"
 
   url "https://github.com/L1cardo/AppTracker-Release/releases/download/v#{version}/App.Tracker.zip"
-  appcast "https://github.com/L1cardo/AppTracker-Release/releases.atom"
+  # appcast "https://github.com/L1cardo/AppTracker-Release/releases.atom"
   name "App Tracker"
   homepage "https://github.com/L1cardo/AppTracker-Release"
 
   depends_on macos: ">= :monterey"
 
   app "App Tracker.app"
+  
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   zap trash: [
     "~/Library/Preferences/com.licardo.AppTracker.plist",
