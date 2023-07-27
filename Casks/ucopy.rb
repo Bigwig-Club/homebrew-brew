8fd76d0301306fc3259ca505a5cde26e3abdfca4
Uclort <uclort@gmail.com>
2023-07-27 10:28:39 +0800

    Update ucopy.rb

--- Casks/ucopy.rb
+++ Casks/ucopy.rb
@@ -5,11 +5,16 @@ cask "ucopy" do
   sha256 "5759d911b38f2c7295f9d0fa1dd78bb591f67f3aca7b701e5c5aea37c7288e95"
 
   url "https://github.com/FaiChou/uCopy/releases/download/v#{version}/uCopy.zip"
-  appcast "https://github.com/FaiChou/uCopy/releases.atom"
+  # appcast "https://github.com/FaiChou/uCopy/releases.atom"
   name "uCopy"
   homepage "https://github.com/FaiChou/uCopy"
 
   depends_on macos: ">= :ventura"
+  
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "uCopy.app"
 
