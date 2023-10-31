fa00870eb14380aacc9fdc949a9b4d9114f3b9c1
Uclort <uclort@gmail.com>
2023-10-31 23:51:32 +0800

    Update appinfo.rb
    更新 0.1.1 版本

--- Casks/appinfo.rb
+++ Casks/appinfo.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 cask "appinfo" do
-  version "0.1.0"
-  sha256 "42f5a40cffe805b40abc164b41a1eb941eca3fc8468d500a2c5713db61dd0e41"
+  version "0.1.1"
+  sha256 "ce4beafa2dca126b9ba1a3c62c692428bf44450d2621760fbeb33e287c6ba775"
 
   url "https://github.com/uclort/AppInfo-Publish/releases/download/#{version}/AppInfo.zip"
   # appcast "https://github.com/uclort/AppInfo-Publish/releases.atom"
