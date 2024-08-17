38ddda134a7f49ff78d2fbd6eb2143e723414796
Uclort <uclort@gmail.com>
2024-08-17 16:29:44 +0800

    Update appinfo.rb
    更新 2.0.0 版本

--- Casks/appinfo.rb
+++ Casks/appinfo.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 cask "appinfo" do
-  version "0.1.1"
-  sha256 "ce4beafa2dca126b9ba1a3c62c692428bf44450d2621760fbeb33e287c6ba775"
+  version "2.0.0"
+  sha256 "405a3d70ee473db365299ab13b2c09b27d8835a2509318d8562301c513ee90f8"
 
   url "https://github.com/uclort/AppInfo-Publish/releases/download/#{version}/AppInfo.zip"
   # appcast "https://github.com/uclort/AppInfo-Publish/releases.atom"
@@ -19,6 +19,6 @@ cask "appinfo" do
   app "AppInfo.app"
 
   zap trash: [
-    "~/Library/Containers/uclort.com.AppInfo-macOS/Data/Library/Preferences/uclort.com.AppInfo-macOS.plist",
+    "~/Library/Containers/uclort.com.AppInfo/Data/Library/Preferences/uclort.com.AppInfo.plist",
   ]
 end
